58c7327d699a966bc4960ee7fe9af69cce323e23
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-10-10 01:47:14 +0000

    Updates on css jquery

--- bootstrap/style.php
+++ bootstrap/style.php
@@ -1,88 +1,31 @@
-<link rel="stylesheet" href="../design/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="../design/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../design/bower_components/font-awesome/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="../design/bower_components/Ionicons/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../design/dist/css/AdminLTE.min.css">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="../design/dist/css/skins/_all-skins.min.css">
+    <!-- Morris chart -->
+    <link rel="stylesheet" href="../design/bower_components/morris.js/morris.css">
+    <!-- jvectormap -->
+    <link rel="stylesheet" href="../design/bower_components/jvectormap/jquery-jvectormap.css">
+    <!-- Date Picker -->
+    <link rel="stylesheet" href="../design/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="../design/bower_components/bootstrap-daterangepicker/daterangepicker.css">
+    <!-- bootstrap wysihtml5 - text editor -->
+    <link rel="stylesheet" href="../design/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
 
-<link rel="stylesheet" href="../design/bower_components/font-awesome/css/font-awesome.min.css">
-
-<link rel="stylesheet" href="../design/bower_components/Ionicons/css/ionicons.min.css">
-
-<link rel="stylesheet" href="../design/dist/css/AdminLTE.min.css">
-
-<link rel="stylesheet" href="../design/dist/css/skins/_all-skins.min.css">
-
-
-<!--[if lt IE 9]>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
 
-<link rel="stylesheet"
-    href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-
-<script defer="" referrerpolicy="origin"
-    src="/cdn-cgi/zaraz/s.js?z=JTdCJTIyZXhlY3V0ZWQlMjIlM0ElNUIlNUQlMkMlMjJ0JTIyJTNBJTIyQWRtaW5MVEUlMjAyJTIwJTdDJTIwVG9wJTIwTmF2aWdhdGlvbiUyMiUyQyUyMnglMjIlM0EwLjIwMjU4NTY3NjUwMzUzNTY1JTJDJTIydyUyMiUzQTE1MzYlMkMlMjJoJTIyJTNBODY0JTJDJTIyaiUyMiUzQTY1MCUyQyUyMmUlMjIlM0ExNTM2JTJDJTIybCUyMiUzQSUyMmh0dHBzJTNBJTJGJTJGYWRtaW5sdGUuaW8lMkZ0aGVtZXMlMkZBZG1pbkxURSUyRnBhZ2VzJTJGbGF5b3V0JTJGdG9wLW5hdi5odG1sJTIyJTJDJTIyciUyMiUzQSUyMmh0dHBzJTNBJTJGJTJGYWRtaW5sdGUuaW8lMkZ0aGVtZXMlMkZBZG1pbkxURSUyRnBhZ2VzJTJGbGF5b3V0JTJGYm94ZWQuaHRtbCUyMiUyQyUyMmslMjIlM0EyNCUyQyUyMm4lMjIlM0ElMjJVVEYtOCUyMiUyQyUyMm8lMjIlM0EtNDgwJTJDJTIycSUyMiUzQSU1QiU1RCU3RA==">
-</script>
-
-<script nonce="8f32b871-3021-4bad-8131-84e098cbc9e0">
-    (function(w, d) {
-        ! function(bt, bu, bv, bw) {
-            bt[bv] = bt[bv] || {};
-            bt[bv].executed = [];
-            bt.zaraz = {
-                deferred: [],
-                listeners: []
-            };
-            bt.zaraz.q = [];
-            bt.zaraz._f = function(bx) {
-                return function() {
-                    var by = Array.prototype.slice.call(arguments);
-                    bt.zaraz.q.push({
-                        m: bx,
-                        a: by
-                    })
-                }
-            };
-            for (const bz of ["track", "set", "debug"]) bt.zaraz[bz] = bt.zaraz._f(bz);
-            bt.zaraz.init = () => {
-                var bA = bu.getElementsByTagName(bw)[0],
-                    bB = bu.createElement(bw),
-                    bC = bu.getElementsByTagName("title")[0];
-                bC && (bt[bv].t = bu.getElementsByTagName("title")[0].text);
-                bt[bv].x = Math.random();
-                bt[bv].w = bt.screen.width;
-                bt[bv].h = bt.screen.height;
-                bt[bv].j = bt.innerHeight;
-                bt[bv].e = bt.innerWidth;
-                bt[bv].l = bt.location.href;
-                bt[bv].r = bu.referrer;
-                bt[bv].k = bt.screen.colorDepth;
-                bt[bv].n = bu.characterSet;
-                bt[bv].o = (new Date).getTimezoneOffset();
-                if (bt.dataLayer)
-                    for (const bG of Object.entries(Object.entries(dataLayer).reduce(((bH, bI) => ({
-                            ...bH[1],
-                            ...bI[1]
-                        })), {}))) zaraz.set(bG[0], bG[1], {
-                        scope: "page"
-                    });
-                bt[bv].q = [];
-                for (; bt.zaraz.q.length;) {
-                    const bJ = bt.zaraz.q.shift();
-                    bt[bv].q.push(bJ)
-                }
-                bB.defer = !0;
-                for (const bK of [localStorage, sessionStorage]) Object.keys(bK || {}).filter((bM => bM
-                    .startsWith("_zaraz_"))).forEach((bL => {
-                    try {
-                        bt[bv]["z_" + bL.slice(7)] = JSON.parse(bK.getItem(bL))
-                    } catch {
-                        bt[bv]["z_" + bL.slice(7)] = bK.getItem(bL)
-                    }
-                }));
-                bB.referrerPolicy = "origin";
-                bB.src = "/cdn-cgi/zaraz/s.js?z=" + btoa(encodeURIComponent(JSON.stringify(bt[bv])));
-                bA.parentNode.insertBefore(bB, bA)
-            };
-            ["complete", "interactive"].includes(bu.readyState) ? zaraz.init() : bt.addEventListener(
-                "DOMContentLoaded", zaraz.init)
-        }(w, d, "zarazData", "script");
-    })(window, document);
-    </script>
+    <!-- Google Font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
