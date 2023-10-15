e5c15cd9122974db354c659c451f42e8c6b028ac
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-15 20:45:34 +0800

    xxx

--- CommonCode/header.php
+++ CommonCode/header.php
@@ -151,7 +151,7 @@ if ($_SESSION['teacher'] !== true || $_SESSION['admin'] !== false) {
                             <img src="../design/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
 
                             <span class="hidden-xs" id="teacher_name_main">
-                                <?php include_once "../Database/DisplayUserInfo.php";
+                                <?php include "../Database/DisplayUserInfo.php";
                                 $displayUserInfo = new DisplayUserInfo();
                                 $sql = "SELECT * FROM user_info_view WHERE user_info_id = '{$_SESSION['id']}'";
                                 $displayUserInfo->displayTeacherName($sql);
