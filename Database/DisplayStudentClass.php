5ec6f266707fb1d13b76c96b76716ee8f887526f
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-16 19:05:26 +0800

    Added Student table

---
+++ Database/DisplayStudentClass.php
@@ -0,0 +1,36 @@
+<?php 
+include_once "Connection.php";
+class DisplayStudentClass extends Connection{
+    function __construct(){
+        parent :: __construct();
+    }
+
+    function ActiveStudentList(){
+        $table = "user_info_view";
+        $sql = "SELECT * FROM $table WHERE status = 'Active' AND user_level_description = 'Learner';";  
+        $result = $this->getConnection()->query($sql);
+        if($result->num_rows > 0){
+            while($row = $result->fetch_assoc()){
+                echo "<tr>";
+                echo "<td><a href='#'><span class='glyphicon glyphicon-info-sign' style = 'padding-left: 10px;'></span></a>";
+
+                echo "<td>".$row['personal_id']."</td>";
+                echo "<td>".$row['personal_id']."</td>";
+                echo "<td>".$row['first_name']."</td>";
+                echo "<td>".$row['last_name']. "</td>";
+                echo "<td>".$row['gender']."</td>";
+                echo "<td>".$row['status']."</td>";
+
+                echo "<td>";
+                echo "<a href='#' class='edit' data-toggle='modal' data-target='#editStudentModal' data-id='".$row["user_info_id"]."' style='margin-right:10px; color: text-success;'><span class='glyphicon glyphicon-edit' ></span></a>";
+                
+                echo " <a href='#' class='actvIconBtn text-danger' data-toggle='modal' data-target='#arcviveStudentModal' data-id='".$row["user_info_id"]."'> <span class='glyphicon glyphicon-trash'></span></a>";
+                echo "</td>";
+
+                echo "</tr>";
+            }
+        }
+
+    }
+}
+?>
---
+++ PagesContent/StudentContentFolder/StudentActiveContentMain.php
@@ -0,0 +1,38 @@
+<div class="box box-default">
+    <div class="box-header with-border">
+        <div class="row">
+            <div class="col-xs-6">
+                <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#add-user">
+                    <i class="fa fa-plus"></i> <span>Add Student</span>
+                </button>
+            </div>
+            <div class="col-xs-6">
+                <div class="search-box">
+                    <i class="fa fa-search"></i>
+                    <input type="text" id="userInput" class="form-control" placeholder="Search..">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="box-body" style="overflow-y: scroll; max-height: 400px;">
+        <table id="example2" class="table table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Student ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Gender</th>
+                    <th>Acount Status</th>
+                    <th colspan="2"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php include_once("../Database/DisplayStudentClass.php");
+                    $displayActiveStudent = new DisplayStudentClass();
+                    $displayActiveStudent->ActiveStudentList();
+                ?>
+            </tbody>
+        </table>
+    </div>
+</div>
