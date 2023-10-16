5fbe06fdb8fa3ff9d75028833159288227d2b6fa
Glen-Capstone <133980393+Glen-Capstone@users.noreply.github.com>
2023-10-16 05:17:58 +0000

    sss

--- PagesContent/MainContentFolder/MainContent.php
+++ PagesContent/MainContentFolder/MainContent.php
@@ -1,3 +1,7 @@
+<style>
+var teacher_name=$('#teacher_name_main').text();
+$('#teacher_name_dashboard').text('Hi! '+teacher_name);
+</style>
 <div class="box box-default">
     <div class="box-header with-border">
         <h3 class="box-title" id="teacher_name_dashboard"></h3>
