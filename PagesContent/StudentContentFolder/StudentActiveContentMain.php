30696f824f18c5799c21c7210f65b28519dcfce9
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-16 19:13:53 +0800

    added container

--- PagesContent/StudentContentFolder/StudentActiveContentMain.php
+++ PagesContent/StudentContentFolder/StudentActiveContentMain.php
@@ -15,24 +15,26 @@
         </div>
     </div>
     <div class="box-body container" style="overflow-y: scroll; max-height: 400px;">
-        <table id="example2" class="table table-bordered table-hover text-center">
-            <thead>
-                <tr>
-                    <th></th>
-                    <th>Student ID</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Gender</th>
-                    <th>Acount Status</th>
-                    <th colspan="2"></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php include_once("../Database/DisplayStudentClass.php");
+        <div class="container">
+            <table id="example2" class="table table-bordered table-hover text-center">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Student ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Gender</th>
+                        <th>Acount Status</th>
+                        <th colspan="2"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php include_once("../Database/DisplayStudentClass.php");
                     $displayActiveStudent = new DisplayStudentClass();
                     $displayActiveStudent->ActiveStudentList();
                 ?>
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
