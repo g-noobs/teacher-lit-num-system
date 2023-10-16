25e3c0e9a7872d39fd5a3b00d1832eb3108a0bd4
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-16 19:15:22 +0800

    sss

--- PagesContent/StudentContentFolder/StudentActiveContentMain.php
+++ PagesContent/StudentContentFolder/StudentActiveContentMain.php
@@ -14,27 +14,25 @@
             </div>
         </div>
     </div>
-    <div class="box-body container" style="overflow-y: scroll; max-height: 400px;">
-        <div class="container">
-            <table id="example2" class="table table-bordered table-hover text-center">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Student ID</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Gender</th>
-                        <th>Acount Status</th>
-                        <th colspan="2"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php include_once("../Database/DisplayStudentClass.php");
+    <div class="box-body container-fluid" style="overflow-y: scroll; max-height: 400px;">
+        <table id="example2" class="table table-bordered table-hover text-center">
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Student ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Gender</th>
+                    <th>Acount Status</th>
+                    <th colspan="2"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php include_once("../Database/DisplayStudentClass.php");
                     $displayActiveStudent = new DisplayStudentClass();
                     $displayActiveStudent->ActiveStudentList();
                 ?>
-                </tbody>
-            </table>
-        </div>
+            </tbody>
+        </table>
     </div>
 </div>
