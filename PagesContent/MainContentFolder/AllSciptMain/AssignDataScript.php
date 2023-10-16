02606eeb31194cdd36f15bd5c1cfaac5b8868f91
Glen-Capstone <133980393+Glen-Capstone@users.noreply.github.com>
2023-10-16 05:28:32 +0000

    ttt

--- PagesContent/MainContentFolder/AllSciptMain/AssignDataScript.php
+++ PagesContent/MainContentFolder/AllSciptMain/AssignDataScript.php
@@ -1,7 +1,7 @@
 <script>
 $(function() {
     var teacher_name = $('#teacher_name_main').text();
-    $('#teacher_name_dashboard').text('Hi! ' + teacher_name);
+    $('#teacher_name_dashboard').text('Good Day! Teacher ' + teacher_name);
 });
 </script>
 
