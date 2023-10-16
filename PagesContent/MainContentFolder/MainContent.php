2895b30bb11c057f79e96e064a6e3fe07b14202a
Glen-Capstone <133980393+Glen-Capstone@users.noreply.github.com>
2023-10-16 05:23:26 +0000

    xxx

--- PagesContent/MainContentFolder/MainContent.php
+++ PagesContent/MainContentFolder/MainContent.php
@@ -1,12 +1,9 @@
-<style>
-var teacher_name=$('#teacher_name_main').text();
-$('#teacher_name_dashboard').text('Hi! '+teacher_name);
-</style>
+
 <div class="box box-default">
     <div class="box-header with-border">
         <h3 class="box-title" id="teacher_name_dashboard"></h3>
     </div>
     <div class="box-body" id="content_body">
-        
+        test for boday
     </div>
 </div>
