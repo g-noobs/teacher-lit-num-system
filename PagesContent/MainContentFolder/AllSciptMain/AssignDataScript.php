c97ba067f0b14006a3851365454be2d0b0a45917
Glen-Capstone <133980393+Glen-Capstone@users.noreply.github.com>
2023-10-16 05:11:21 +0000

    xxx

--- PagesContent/MainContentFolder/AllSciptMain/AssignDataScript.php
+++ PagesContent/MainContentFolder/AllSciptMain/AssignDataScript.php
@@ -1,8 +1,5 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <style>
-$(function() {
-    var teacher_name=$('#teacher_name_main').text();
-    $('#teacher_name_dashboard').text('Hi! '+teacher_name);
-}
-);
+var teacher_name=$('#teacher_name_main').text();
+$('#teacher_name_dashboard').text('Hi! '+teacher_name);
 </style>
