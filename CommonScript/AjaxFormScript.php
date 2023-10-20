123696901ace85a53941f51eecb92b29ade388c3
Glen-Capstone <133980393+Glen-Capstone@users.noreply.github.com>
2023-10-20 03:27:20 +0000

    xx

--- CommonScript/AjaxFormScript.php
+++ CommonScript/AjaxFormScript.php
@@ -1,38 +1,26 @@
 
-$.ajax( {
-        url: actionUrl,
-        type: "POST",
-        data: formData,
-        processData: false,
-        contentType: false,
-        success: function(response) {
-            var responseData=JSON.parse(response);
-            // Check if the form submission was successful
-            if (responseData.hasOwnProperty('success')) {
-                $hideModal.modal('hide');
-                $('#successAlert').text(responseData.success);
-                $('#successBanner').show();
-                setTimeout(function() {
-                        $("#successBanner").fadeOut("slow");
-                        location.reload();
-                    },1500);
-                
-            }
-            else if (responseData.hasOwnProperty('error')) {
-                $hideModal.modal('hide');
-                $('#errorAlert').text(responseData.error);
-                $('#errorBanner').show();
-
-                setTimeout(function() {
-                        $("#errorBanner").fadeOut("slow");
-                        location.reload();
-                    }, 1500);
-            }
-        },
-        error: function() {
+$.ajax({
+    url: actionUrl,
+    type: "POST",
+    data: formData,
+    processData: false,
+    contentType: false,
+    success: function(response) {
+        var responseData=JSON.parse(response);
+        // Check if the form submission was successful
+        if (responseData.hasOwnProperty('success')) {
             $hideModal.modal('hide');
-            //show alert banner id = errorBanner
-            $('#errorAlert').text('An error occurred during the AJAX request.');
+            $('#successAlert').text(responseData.success);
+            $('#successBanner').show();
+            setTimeout(function() {
+                    $("#successBanner").fadeOut("slow");
+                    location.reload();
+                },1500);
+            
+        }
+        else if (responseData.hasOwnProperty('error')) {
+            $hideModal.modal('hide');
+            $('#errorAlert').text(responseData.error);
             $('#errorBanner').show();
 
             setTimeout(function() {
@@ -40,5 +28,16 @@ $.ajax( {
                     location.reload();
                 }, 1500);
         }
+    },
+    error: function() {
+        $hideModal.modal('hide');
+        //show alert banner id = errorBanner
+        $('#errorAlert').text('An error occurred during the AJAX request.');
+        $('#errorBanner').show();
+
+        setTimeout(function() {
+                $("#errorBanner").fadeOut("slow");
+                location.reload();
+            }, 1500);
     }
-);
+});
