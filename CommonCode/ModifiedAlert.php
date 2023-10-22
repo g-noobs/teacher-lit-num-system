a2c27527039f9634fa3877004b6d81e450be805f
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-22 12:18:59 +0800

    asd

--- CommonCode/ModifiedAlert.php
+++ CommonCode/ModifiedAlert.php
@@ -5,7 +5,7 @@
     opacity: 0.75;
     position: fixed;
     /* Make it a fixed position */
-    top: 1;
+    top: 50;
     /* Align to the top of the viewport */
     right: 0;
     /* Align to the left of the viewport */
