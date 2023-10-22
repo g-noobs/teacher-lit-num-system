e55926d9babf4947fbb65a849cd5aa3ba4762670
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-22 11:47:08 +0800

    asd

--- CommonScript/AjaxFormScript.php
+++ CommonScript/AjaxFormScript.php
@@ -1,43 +1,42 @@
-
 $.ajax({
-    url: actionUrl,
-    type: "POST",
-    data: formData,
-    processData: false,
-    contentType: false,
-    success: function(response) {
-        var responseData=JSON.parse(response);
-        // Check if the form submission was successful
-        if (responseData.hasOwnProperty('success')) {
-            $hideModal.modal('hide');
-            $('#successAlert').text(responseData.success);
-            $('#successBanner').show();
-            setTimeout(function() {
-                    $("#successBanner").fadeOut("slow");
-                    location.reload();
-                },1500);
-            
-        }
-        else if (responseData.hasOwnProperty('error')) {
-            $hideModal.modal('hide');
-            $('#errorAlert').text(responseData.error);
-            $('#errorBanner').show();
+            url: actionUrl,
+            type: "POST",
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(response) {
+                var responseData = JSON.parse(response);
+                // Check if the form submission was successful
+                if (responseData.hasOwnProperty('success')) {
+                    $hideModal.modal('hide');
+                    $('#successAlert').text(responseData.success);
+                    $('#successBanner').show();
+                    setTimeout(function() {
+                        $("#successBanner").fadeOut("slow");
+                        location.reload();
+                    }, 1500);
 
-            setTimeout(function() {
+
+                    // You can redirect to a different page or perform other actions here
+                } else if (responseData.hasOwnProperty('error')) {
+                    $hideModal.modal('hide');
+                    $('#errorAlert').text(responseData.error);
+                    $('#errorBanner').show();
+                    setTimeout(function() {
+                        $("#errorBanner").fadeOut("slow");
+                        location.reload();
+                    }, 1500);
+                }
+            },
+            error: function() {
+                $hideModal.modal('hide');
+                //show alert banner id = errorBanner
+                $('#errorAlert').text('An error occurred during the AJAX request.');
+                $('#errorBanner').show();
+                setTimeout(function() {
                     $("#errorBanner").fadeOut("slow");
                     location.reload();
                 }, 1500);
-        }
-    },
-    error: function() {
-        $hideModal.modal('hide');
-        //show alert banner id = errorBanner
-        $('#errorAlert').text('An error occurred during the AJAX request.');
-        $('#errorBanner').show();
+            }
 
-        setTimeout(function() {
-                $("#errorBanner").fadeOut("slow");
-                location.reload();
-            }, 1500);
-    }
-});
+        });
--- PagesContent/StudentContentFolder/StudentScriptFolder/AddStudentScript.php
+++ PagesContent/StudentContentFolder/StudentScriptFolder/AddStudentScript.php
@@ -3,6 +3,7 @@ $(function(){
     $('#add_student_form').on('submit', function(e){
         e.preventDefault();
         var formData = new FormData(this);
+        
         var $hideModal = $('#add_student_modal');
         var actionUrl = '../PagesContent/StudentContentFolder/ActionStudent/ActionAddStudent.php';
 
