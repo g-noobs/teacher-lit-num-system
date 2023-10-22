c27966fd2c3ca444424e46691f2aa431cadd7aff
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-22 13:28:37 +0800

    ASD

--- PagesContent/StudentContentFolder/StudentScriptFolder/AddStudentScript.php
+++ PagesContent/StudentContentFolder/StudentScriptFolder/AddStudentScript.php
@@ -7,7 +7,38 @@ $(function(){
         var $hideModal = $('#add_student_modal');
         var actionUrl = '../PagesContent/StudentContentFolder/ActionStudent/ActionAddStudent.php';
 
-        <?php include_once "../CommonScript/AjaxFormScript.php"?>
+        $.ajax({
+            url: actionUrl,
+            type: "POST",
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(response) {
+                var responseData = JSON.parse(response);
+                // Check if the form submission was successful
+                if (responseData.hasOwnProperty('success')) {
+                    $hideModal.modal('hide');
+                    $('#successAlert').text(responseData.success);
+                    $('#successBanner').show();
+                    setTimeout(function() {
+                        $("#successBanner").fadeOut("slow");
+                        //location.reload();
+                    }, 1500);
+
+
+                    // You can redirect to a different page or perform other actions here
+                } else if (responseData.hasOwnProperty('error')) {
+                    $hideModal.modal('hide');
+                    $('#errorAlert').text(responseData.error);
+                    $('#errorBanner').show();
+                    setTimeout(function() {
+                        $("#errorBanner").fadeOut("slow");
+                        //location.reload();
+                    }, 1500);
+                }
+            }
+
+        });
     });
 });
 </script>
