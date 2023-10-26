685d8e9caaf9b257521027905559b8397df73c2d
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-26 16:32:26 +0800

    asd

--- pages/quiz.php
+++ pages/quiz.php
@@ -41,7 +41,29 @@
                         <li class="active">Top Navigation</li>
                     </ol> -->
                 </section>
+                <br>
+                <section>
+                    <div class="align-items-start">
+                        <div class="col-sm-2">
+                            <div class="custom-dropdown">
 
+                                <button class="custom-dropdown-toggle btn" type="button" data-toggle="dropdown"
+                                    style="width:150px; border: 2px solid #E58A00; border-radius:10px; color: #E58A00;">
+                                    <b>Active Quiz</b> <!-- Updated the button text -->
+                                    <span class="caret"></span>
+                                </button>
+                                <ul class="dropdown-menu custom-dropdown-menu">
+                                    <li><a href="#" data-quiz-type="active-quiz"><b>All Active quiz</b></a></li>
+                                    <li><a href="#" data-quiz-type="arch-quiz"><b>All Archive quiz</b></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-sm-10">
+                        </div>
+                    </div>
+                </section>
+                <br>
+                <br>
                 <!-- Main Content-->
                 <section class="content" id="main-content">
                     <?php include_once "../PagesContent/QuizContentFolder/QuizTable/ActiveQuizTable.php"?>
