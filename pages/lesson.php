abccd4def951bb5c0f5aaedb37798cd078adad9e
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-10-26 15:57:35 +0800

    sdfds

--- pages/lesson.php
+++ pages/lesson.php
@@ -44,30 +44,24 @@
                     </ol> -->
                 </section>
 
-                <!-- Main Content-->
-                <section class="content" id="main-content">
-                    <div class="box box-default">
-                        <div class="box-header with-border">
-                            <br>
-                            <div class="row">
-                                <div class="col-xs-6">
-                                    <button type="button" class="btn btn-warning" data-toggle="modal"
-                                        data-target="#add-user">
-                                        <i class="fa fa-plus"></i> <span>Add Lesson</span>
-                                    </button>
-                                </div>
-                                <div class="col-xs-6">
-                                    <div class="search-box">
-                                        <i class="fa fa-search"></i>
-                                        <input type="text" id="userInput" class="form-control" placeholder="Search..">
-                                    </div>
-                                </div>
+                <section>
+                    <div class="row">
+                        <div class="col-xs-6">
+                            <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#add-user">
+                                <i class="fa fa-plus"></i> <span>Add Lesson</span>
+                            </button>
+                        </div>
+                        <div class="col-xs-6">
+                            <div class="search-box">
+                                <i class="fa fa-search"></i>
+                                <input type="text" id="userInput" class="form-control" placeholder="Search..">
                             </div>
                         </div>
-                        <div class="box-body" id="content_body">
-                            test for boday
-                        </div>
                     </div>
+                </section>
+
+                <!-- Main Content-->
+                <section class="content" id="main-content">
                 </section>
 
             </div>
