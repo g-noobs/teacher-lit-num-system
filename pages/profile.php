17eeee0106a59513a02ab449d07c52fa9b3a597f
officeAccount1 <149283331+officeAccount1@users.noreply.github.com>
2023-10-29 05:16:18 +0000

    dfg

---
+++ pages/profile.php
@@ -0,0 +1,57 @@
+<html style="height: auto; min-height: 100%;">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Teacher Portal</title>
+
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+    <?php include_once("../bootstrap/style.php");?>
+    <!-- jQuery 3 -->
+    <script src="../design/bower_components/jquery/dist/jquery.min.js"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="../design/bower_components/jquery-ui/jquery-ui.min.js"></script>
+</head>
+
+<body class="skin-blue layout-top-nav" data-new-gr-c-s-check-loaded="14.1131.0" data-gr-ext-installed
+    style="height: auto; min-height: 100%;">
+
+
+    <div class="wrapper" style="height: auto; min-height: 100%;">
+        <?php include_once("../CommonCode/header.php");?>
+
+        <div class="content-wrapper" style="min-height: 606.2px;">
+            <?php include_once "../CommonCode/ModifiedAlert.php";?>
+
+            <div class="container">
+                <section class="content-header">
+                    <h1>
+                        Teacher Profile
+                        <small>Edit Teacher Data Here</small>
+                    </h1>
+                </section>
+
+
+                <section class="content" id="main-content">
+                    
+                </section>
+
+            </div>
+        </div>
+        <?php 
+        include_once("../CommonCode/footer.php");
+        ?>
+    </div>
+
+    <?php include_once("../bootstrap/jquery.php");?>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- This Script Contain Common Script used on other pages  -->
+    <?php include_once "../CommonScript/CommonAllScript.php";?>
+
+    <!-- // Assign Data and Modified Data in Main or dashboard -->
+    <?php include_once "../PagesContent/MainContentFolder/AllSciptMain/AssignDataScript.php"; ?>
+</body>
+
+</html>
--- pieChart.php
+++ pieChart.php
@@ -145,3 +145,33 @@ $(function() {
     pieChart.Doughnut(PieData, pieOptions)
 });
 </script>
+
+<!-- Morris Donut Chart -->
+<script>
+$(function() {
+    var admin_count = "";
+    var teacher_count = "";
+    var student_count = "";
+    
+    //DONUT CHART
+    var donut = new Morris.Donut({
+        element: 'sales-chart',
+        resize: true,
+        colors: ["#3c8dbc", "#f56954", "#00a65a"],
+        data: [{
+                label: "Download Sales",
+                value: 12
+            },
+            {
+                label: "In-Store Sales",
+                value: 30
+            },
+            {
+                label: "Mail-Order Sales",
+                value: 20
+            }
+        ],
+        hideHover: 'auto'
+    });
+});
+</script>
