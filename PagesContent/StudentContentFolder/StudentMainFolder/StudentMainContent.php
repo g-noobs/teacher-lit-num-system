36258631567b1c9e63eef90f2cebe62a4ac4adcb
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-11-13 19:11:30 +0800

    asd

--- PagesContent/StudentContentFolder/StudentMainFolder/StudentMainContent.php
+++ PagesContent/StudentContentFolder/StudentMainFolder/StudentMainContent.php
@@ -40,7 +40,7 @@ $(function() {
                         <br>
                         <div class="row">
                         <div class="col-xs-6">
-                            <button type="button" class="btn btn-primary add-student" data-class-id="${tab.id}">
+                            <button type="button" class="btn btn-primary add-student" data-toggle='modal' data-target='#add_student_modal' data-class-id="${tab.id}">
                             <i class="fa fa-plus"></i> <span>Add Student</span>
                             </button>
                         </div>
