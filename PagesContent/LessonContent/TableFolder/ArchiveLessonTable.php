2391e84ffbbded1d7862d27a3ccee37e9545b63a
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-11-14 06:52:50 +0000

    asd

--- PagesContent/LessonContent/TableFolder/ArchiveLessonTable.php
+++ PagesContent/LessonContent/TableFolder/ArchiveLessonTable.php
@@ -5,10 +5,10 @@
             <br>
             <div class="row">
                 <div class="col-xs-6">
-                    <button type="button" class="btn btn-warning btn-sm" data-toggle='modal'
+                    <!-- <button type="button" class="btn btn-warning btn-sm" data-toggle='modal'
                         data-target="#addLessonModal">
                         <i class="fa fa-plus"></i> <span> Add Lesson</span>
-                    </button>
+                    </button> -->
                 </div>
                 <div class="col-xs-6">
                     <div class="search-box">
