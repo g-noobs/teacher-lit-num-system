be468fadd2039ad2e8ad37359091db8c9313d4fd
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-11-14 23:39:21 +0000

    asd

--- PagesContent/StudentContentFolder/StudentScriptFolder/AddStudentScript.php
+++ PagesContent/StudentContentFolder/StudentScriptFolder/AddStudentScript.php
@@ -1,75 +1,85 @@
 <script>
-$(function() {
-    $("#addUserForm").on("submit", function(e) {
-        e.preventDefault();
+(document).ready(function() {
+    $(document).on('click', '.add_stdnt_btn', function() {
+        $('#add_student_modal').modal('show');
 
-        var formData = new FormData(this);
-        console.log(this);
+        var class_id = $(this).data('class-id');
 
-        var $hideModal = $('#add_user_modal');
-        var actionUrl = '../PagesContent/UserContent/ActionsUsers/ActionAddTeacher.php';
+        $('#add_student_modal').find('.modal-title').text('Add Student for Class ID: ' + class_id);
 
-        $.ajax({
-            url: actionUrl,
-            type: "POST",
-            data: formData,
-            processData: false,
-            contentType: false,
-            dataType: 'json',
-            success: function(response) {
-                // Check if the response contains an array of errors
-                if (Array.isArray(response)) {
-                    // Clear previous error messages
-                    $("#add_user_modal_alert_text").empty();
-                    $("#add_user_modal_alert").show();
+        $("#addUserForm").on("submit", function(e) {
+            e.preventDefault();
 
-                    // Update the element with the received errors
-                    $.each(response, function(index, error) {
-                        $("#add_user_modal_alert_text").append("<p class='error'>" +
-                            error +
-                            "</p><br>");
-                        console.log(error);
-                    });
+            var formData = new FormData(this);
+            console.log(this);
 
-                    setTimeout(function() {
-                        $("#add_user_modal_alert").fadeOut("slow");
+            var $hideModal = $('#add_user_modal');
+            var actionUrl = '../PagesContent/UserContent/ActionsUsers/ActionAddStudent.php';
 
-                    }, 3500);
-                } else {
-                    // Check if the form submission was successful
-                    if (response.hasOwnProperty('success')) {
-                        $hideModal.modal('hide');
-                        $('#successAlert').text(response.success);
-                        $('#successBanner').show();
+            $.ajax({
+                url: actionUrl,
+                type: "POST",
+                data: formData,
+                processData: false,
+                contentType: false,
+                dataType: 'json',
+                success: function(response) {
+                    // Check if the response contains an array of errors
+                    if (Array.isArray(response)) {
+                        // Clear previous error messages
+                        $("#add_user_modal_alert_text").empty();
+                        $("#add_user_modal_alert").show();
+
+                        // Update the element with the received errors
+                        $.each(response, function(index, error) {
+                            $("#add_user_modal_alert_text").append(
+                                "<p class='error'>" +
+                                error +
+                                "</p><br>");
+                            console.log(error);
+                        });
+
                         setTimeout(function() {
-                            $("#successBanner").fadeOut("slow");
-                            location.reload();
-                        }, 1500);
+                            $("#add_user_modal_alert").fadeOut("slow");
+
+                        }, 3500);
+                    } else {
+                        // Check if the form submission was successful
+                        if (response.hasOwnProperty('success')) {
+                            $hideModal.modal('hide');
+                            $('#successAlert').text(response.success);
+                            $('#successBanner').show();
+                            setTimeout(function() {
+                                $("#successBanner").fadeOut("slow");
+                                location.reload();
+                            }, 1500);
 
 
 
-                    } else if (response.hasOwnProperty('error')) {
-                        $hideModal.modal('hide');
-                        $('#errorAlert').text(response.error);
-                        $('#errorBanner').show();
-                        setTimeout(function() {
-                            $("#errorBanner").fadeOut("slow");
-                            location.reload();
-                        }, 1500);
+                        } else if (response.hasOwnProperty('error')) {
+                            $hideModal.modal('hide');
+                            $('#errorAlert').text(response.error);
+                            $('#errorBanner').show();
+                            setTimeout(function() {
+                                $("#errorBanner").fadeOut("slow");
+                                location.reload();
+                            }, 1500);
+                        }
                     }
+                },
+                error: function(xhr, status, error) {
+                    console.error(xhr.responseText);
+                    $hideModal.modal('hide');
+                    //show alert banner id = errorBanner
+                    $('#errorAlert').text(
+                        'An error occurred during the AJAX request.');
+                    $('#errorBanner').show();
+                    setTimeout(function() {
+                        $("#errorBanner").fadeOut("slow");
+                        location.reload();
+                    }, 1500);
                 }
-            },
-            error: function(xhr, status, error) {
-                console.error(xhr.responseText);
-                $hideModal.modal('hide');
-                //show alert banner id = errorBanner
-                $('#errorAlert').text('An error occurred during the AJAX request.');
-                $('#errorBanner').show();
-                setTimeout(function() {
-                    $("#errorBanner").fadeOut("slow");
-                    location.reload();
-                }, 1500);
-            }
+            });
         });
     });
 });
