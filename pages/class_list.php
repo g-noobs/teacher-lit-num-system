2100614cd30e9afa8a67309af62cefd3e585dd84
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-11-14 19:59:10 +0800

    asdv

--- pages/class_list.php
+++ pages/class_list.php
@@ -13,11 +13,6 @@
     ?>
     <?php include_once "../CommonCode/ModifiedSearchStyle.php";?>
 
-    <!-- Include jQuery -->
-<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-
-<!-- Include Bootstrap JS -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 </head>
 
