f1491f73fb364cb83f4db15f6d2d787a8c00ff41
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-11-15 04:16:57 +0000

    asd

--- PagesContent/QuizFolder/ActionQuizFolder/ActionViewQuizData.php
+++ PagesContent/QuizFolder/ActionQuizFolder/ActionViewQuizData.php
@@ -12,29 +12,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $sql = "SELECT * FROM $table WHERE quiz_id = '$id' AND quiz_status = 1;";
         $result = $conn->query($sql);
 
-        if($result->num_rows > 0){
-            while($row = $result->fetch_assoc()){
-                if($row['quiz_selectionB'] == null){
-                    $option1 = 'false';
-                }else{
-                    $option1 = $row['quiz_selectionB'];
-                }
-                if($row['quiz_status'] == 1){
-                    $status = 'Active';
-                }else{
-                    $status = 'Inactive';
-                }
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                // Check each value and replace empty ones with null or none
+                $quiz_id = !empty($row['quiz_id']) ? $row['quiz_id'] : null;
+                $quiz_question = !empty($row['quiz_question']) ? $row['quiz_question'] : null;
+                $correct_answer = !empty($row['quiz_selectionA']) ? $row['quiz_selectionA'] : null;
+                $option1 = !empty($row['quiz_selectionB']) ? $row['quiz_selectionB'] : null;
+                $option2 = !empty($row['quiz_selectionC']) ? $row['quiz_selectionC'] : null;
+                $option3 = !empty($row['quiz_selectionD']) ? $row['quiz_selectionD'] : null;
+                $topic_source = !empty($row['topic_id']) ? $row['topic_id'] : null;
+                $date_created = !empty($row['date_created']) ? $row['date_created'] : null;
+                $quiz_status = ($row['quiz_status'] == 1) ? 'Active' : 'Inactive';
 
+                // Create the response array
                 $response = array(
-                    'quiz_id' => $row['quiz_id'],
-                    'quiz_question' => $row['quiz_question'],
-                    'correct_answer' => $row['quiz_selectionA'],
+                    'quiz_id' => $quiz_id,
+                    'quiz_question' => $quiz_question,
+                    'correct_answer' => $correct_answer,
                     'option1' => $option1,
-                    'option2' => $row['quiz_selectionC'],
-                    'option3' => $row['quiz_selectionD'],
-                    'topic_source' => $row['topic_id'],
-                    'date_created' => $row['date_created'],
-                    'quiz_status' => $status,
+                    'option2' => $option2,
+                    'option3' => $option3,
+                    'topic_source' => $topic_source,
+                    'date_created' => $date_created,
+                    'quiz_status' => $quiz_status,
                 );
             }
             echo json_encode($response);
