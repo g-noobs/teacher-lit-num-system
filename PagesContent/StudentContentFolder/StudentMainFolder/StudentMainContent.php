ca4bd3dcaf328737d48d054df0714e9085d1349e
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-11-15 00:56:58 +0000

    dfg

--- PagesContent/StudentContentFolder/StudentMainFolder/StudentMainContent.php
+++ PagesContent/StudentContentFolder/StudentMainFolder/StudentMainContent.php
@@ -4,19 +4,7 @@
 <!-- Add student script -->
 <?php include_once "../PagesContent/StudentContentFolder/StudentScriptFolder/AddStudentScript.php"?>
 
-<!--Script below will be used for search -->
-<script>
-$(document).ready(function() {
-    $(".userInput").on("keyup", function() {
-        var value = $(this).val().toLowerCase();
-        $("tbody tr").filter(function() {
-            var rowText = $(this).text().toLowerCase();
-            var pText = $(this).find("p").text().toLowerCase();
-            $(this).toggle(rowText.indexOf(value) > -1 || pText.indexOf(value) > -1);
-        });
-    });
-});
-</script>
+
 
 <div class="container">
     <!-- <h2>Dynamic Tabs</h2>
@@ -131,3 +119,16 @@ $(document).ready(function() {
 
 
 
+<!--Script below will be used for search -->
+<script>
+$(document).ready(function() {
+    $(".userInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("tbody tr").filter(function() {
+            var rowText = $(this).text().toLowerCase();
+            var pText = $(this).find("p").text().toLowerCase();
+            $(this).toggle(rowText.indexOf(value) > -1 || pText.indexOf(value) > -1);
+        });
+    });
+});
+</script>
