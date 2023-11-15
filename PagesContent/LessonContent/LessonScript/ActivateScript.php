63a9eba2898a681df6894eb7482f5e4b02fd60e3
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-11-15 06:50:49 +0000

    asd

--- PagesContent/LessonContent/LessonScript/ActivateScript.php
+++ PagesContent/LessonContent/LessonScript/ActivateScript.php
@@ -1,7 +1,7 @@
 <script>
 $(document).ready(function() {
 
-    var $modalControl = $('#confirm_activate');
+    var $modalControl = $('#activate_modal');
     // Check or uncheck all checkboxes when the "Select All" checkbox is clicked
     $("#select-all").on('click', function() {
         $(".checkbox").prop("checked", $(this).prop("checked"));
