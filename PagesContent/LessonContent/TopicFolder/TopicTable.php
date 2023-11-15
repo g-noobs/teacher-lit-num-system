26e17122a5400e71e8e875d48a059efca9a019d9
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-11-15 06:21:47 +0000

    xcv

--- PagesContent/LessonContent/TopicFolder/TopicTable.php
+++ PagesContent/LessonContent/TopicFolder/TopicTable.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-xs-12">
-        <div class="box box-warning container">
+        <div class="box box-default container">
             <div class="box-header with-border">
 
                 <h3 class="box-title">Topic List</h3>
