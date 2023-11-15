a5600e1d2b9330b93afeb52b1596ce6ea38731ef
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-11-15 04:19:16 +0000

    remove null

--- PagesContent/QuizFolder/ActionQuizFolder/ActionViewQuizData.php
+++ PagesContent/QuizFolder/ActionQuizFolder/ActionViewQuizData.php
@@ -15,14 +15,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
                 // Check each value and replace empty ones with null or none
-                $quiz_id = !empty($row['quiz_id']) ? $row['quiz_id'] : null;
-                $quiz_question = !empty($row['quiz_question']) ? $row['quiz_question'] : null;
-                $correct_answer = !empty($row['quiz_selectionA']) ? $row['quiz_selectionA'] : null;
-                $option1 = !empty($row['quiz_selectionB']) ? $row['quiz_selectionB'] : null;
-                $option2 = !empty($row['quiz_selectionC']) ? $row['quiz_selectionC'] : null;
-                $option3 = !empty($row['quiz_selectionD']) ? $row['quiz_selectionD'] : null;
-                $topic_source = !empty($row['topic_id']) ? $row['topic_id'] : null;
-                $date_created = !empty($row['date_created']) ? $row['date_created'] : null;
+                $quiz_id = !empty($row['quiz_id']) ? $row['quiz_id'] : 'none';
+                $quiz_question = !empty($row['quiz_question']) ? $row['quiz_question'] : 'none';
+                $correct_answer = !empty($row['quiz_selectionA']) ? $row['quiz_selectionA'] : 'none';
+                $option1 = !empty($row['quiz_selectionB']) ? $row['quiz_selectionB'] : 'none';
+                $option2 = !empty($row['quiz_selectionC']) ? $row['quiz_selectionC'] : 'none';
+                $option3 = !empty($row['quiz_selectionD']) ? $row['quiz_selectionD'] : 'none';
+                $topic_source = !empty($row['topic_id']) ? $row['topic_id'] : 'none';
+                $date_created = !empty($row['date_created']) ? $row['date_created'] : 'none';
                 $quiz_status = ($row['quiz_status'] == 1) ? 'Active' : 'Inactive';
 
                 // Create the response array
