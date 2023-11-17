4e0ce269b1b550a8928ae5153aa7982b0803845c
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-11-17 06:56:41 +0000

    asd

--- PagesContent/QuizFolder/TableQuiz/QuizMainTable.php
+++ PagesContent/QuizFolder/TableQuiz/QuizMainTable.php
@@ -4,7 +4,7 @@
             <div class="box-header with-border">
                 <button id="archive_btn" type="button" class="btn btn-danger" data-toggle="tooltip"
                     title="Archive Class">
-                    <i class="glyphicon glyphicon-trash"></i>
+                    <i class="glyphicon glyphicon-trash"></i><span>Archive Quiz</span>
                 </button>
                 <div class="box-tools pull-right">
                     <div class="search-box" style="margin-right: 35px;">
