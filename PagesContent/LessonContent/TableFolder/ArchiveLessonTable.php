d778b74e8cc229598d41e61b03f55bd94b21b44d
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-11-18 06:46:36 +0000

    sdf

--- PagesContent/LessonContent/TableFolder/ArchiveLessonTable.php
+++ PagesContent/LessonContent/TableFolder/ArchiveLessonTable.php
@@ -1,56 +1,57 @@
-<?php include_once "../../../CommonCode/ModifiedSearchStyle.php";?>
+<?php error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+include_once "../../../CommonCode/ModifiedSearchStyle.php";?>
 
 <!-- Confirmation modal -->
 <?php include_once "../CommonLesson/ConfirmationModal.php";?>
 
-
 <div class="row">
-    <div class="col-xs-12">
-        <div class="box box-default container">
-            <div class="box-header with-border">
-                <button id="activate_btn" type="button" class="btn btn-success" data-toggle="modal" data-target="">
-                    <i class="fa fa-check-circle"></i> <span>Activate</span>
-                </button>
-                <div class="box-tools pull-right">
-                    <div class="search-box" style="margin-right: 35px;">
+    <div class="box container">
+        <div class="box-header">
+            <br>
+            <div class="row">
+                <div class="col-xs-6">
+                    <button id="activate_btn" type="button" class="btn btn-success" data-toggle="modal" data-target="">
+                        <i class="fa fa-check-circle"></i> <span>Activate</span>
+                    </button>
+                </div>
+                <div class="col-xs-6">
+                    <div class="search-box">
                         <i class="fa fa-search"></i>
                         <input type="text" id="userInput" class="form-control" placeholder="Search..">
                     </div>
                 </div>
-                <div class="box-tools pull-right">
-                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                    </button>
-                </div>
             </div>
-            <br>
-            <!-- /.box-header -->
-            <div class="box-body" style="overflow-y: scroll; max-height: 400px;">
-                <div class="table-responsive">
-                    <table id="example2" class="table table-bordered table-hover text-center">
-                        <thead>
-                            <tr>
-                                <th><input type='checkbox' id="select-all" class='checkbox'></th>
-                                <th>Lesson ID</th>
-                                <th>Lesson Description</th>
-                                <th>Category Name</th>
-                                <th>Module Name</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php 
-                        include_once("../../../Database/LessonDisplayClass.php");
-                        $quiztable = new LessonDisplayClass();
-                        $quiztable->archivedLessonTable();
-                        ?>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-            <!-- /.box-body -->
+
         </div>
-        <!-- /.box -->
+        <!-- /.box-header -->
+        <div class="box-body" style="overflow-y: scroll; max-height: 400px;">
+            <div class="table-responsive">
+                <table id="example2" class="table table-bordered table-hover text-center">
+                    <thead>
+                        <tr>
+                            <th><input type='checkbox' id="select-all" class='checkbox'></th>
+                            <th>Lesson ID</th>
+                            <th>Lesson Description</th>
+                            <th>Category Name</th>
+                            <th>Module Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php 
+                        include_once("../../../Database/LessonDisplayClass.php");
+                        $archivedLessonTable = new LessonDisplayClass();
+                        $teacher_user_id = $_SESSION['id'];
+                        $archivedLessonTable->archivelessonTable($teacher_user_id);
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <!-- /.box-body -->
     </div>
-    <!-- /.col -->
+    <!-- /.box -->
 </div>
 
 <!-- activate script -->
@@ -67,6 +68,32 @@ $(document).ready(function() {
             var pText = $(this).find("p").text().toLowerCase();
             $(this).toggle(rowText.indexOf(value) > -1 || pText.indexOf(value) > -1);
         });
+    });
+});
+</script>
+
+
+<!-- custom dropdown -->
+<script>
+$(function() {
+    $('.custom-dropdown-menu a').click(function(e) {
+        e.preventDefault();
+        var categoryType = $(this).data('lesson-type');
+        var contentPath = '';
+
+        if (categoryType === 'active-lesson') {
+            location.reload();
+        } else if (categoryType === 'archive-lesson') {
+            contentPath = '../PagesContent/LessonContent/TableFolder/ArchiveLessonTable.php';
+        }
+        $('.custom-dropdown-toggle').html($(this).text() + '<span class="caret"></span>');
+        if (contentPath !== '') {
+            $("#lesson-table").fadeOut(400, function() {
+                $(this).load(contentPath, function() {
+                    $(this).fadeIn(400);
+                });
+            });
+        }
     });
 });
 </script>
