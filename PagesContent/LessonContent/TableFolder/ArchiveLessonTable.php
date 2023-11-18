41fcfd626a9a3a1b954977376be90f9ed8c0c4c8
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-11-18 06:42:41 +0000

    xcv

--- PagesContent/LessonContent/TableFolder/ArchiveLessonTable.php
+++ PagesContent/LessonContent/TableFolder/ArchiveLessonTable.php
@@ -1,55 +1,56 @@
-
 <?php include_once "../../../CommonCode/ModifiedSearchStyle.php";?>
 
 <!-- Confirmation modal -->
 <?php include_once "../CommonLesson/ConfirmationModal.php";?>
 
+
 <div class="row">
-    <div class="box container">
-        <div class="box-header">
-            <br>
-            <div class="row">
-                <div class="col-xs-6">
-                    <button id="activate_btn" type="button" class="btn btn-success" data-toggle="modal" data-target="">
-                        <i class="fa fa-check-circle"></i> <span>Activate</span>
-                    </button>
-                </div>
-                <div class="col-xs-6">
-                    <div class="search-box">
+    <div class="col-xs-12">
+        <div class="box box-default container">
+            <div class="box-header with-border">
+                <button id="activate_btn" type="button" class="btn btn-success" data-toggle="modal" data-target="">
+                    <i class="fa fa-check-circle"></i> <span>Activate</span>
+                </button>
+                <div class="box-tools pull-right">
+                    <div class="search-box" style="margin-right: 35px;">
                         <i class="fa fa-search"></i>
                         <input type="text" id="userInput" class="form-control" placeholder="Search..">
                     </div>
                 </div>
+                <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                    </button>
+                </div>
             </div>
-
-        </div>
-        <!-- /.box-header -->
-        <div class="box-body" style="overflow-y: scroll; max-height: 400px;">
-            <div class="table-responsive">
-                <table id="example2" class="table table-bordered table-hover text-center">
-                    <thead>
-                        <tr>
-                            <th><input type='checkbox' id="select-all" class='checkbox'></th>
-                            <th>Lesson ID</th>
-                            <th>Lesson Description</th>
-                            <th>Category Name</th>
-                            <th>Module Name</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php 
+            <br>
+            <!-- /.box-header -->
+            <div class="box-body" style="overflow-y: scroll; max-height: 400px;">
+                <div class="table-responsive">
+                    <table id="example2" class="table table-bordered table-hover text-center">
+                        <thead>
+                            <tr>
+                                <th><input type='checkbox' id="select-all" class='checkbox'></th>
+                                <th>Lesson ID</th>
+                                <th>Lesson Description</th>
+                                <th>Category Name</th>
+                                <th>Module Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php 
                         include_once("../../../Database/LessonDisplayClass.php");
-                        $teacher_user_id = $_SESSION['id'];
-                        $archlessonTable = new LessonDisplayClass();
-                        $archlessonTable->lessonTable($teacher_user_id);
+                        $quiztable = new LessonDisplayClass();
+                        $quiztable->archivedLessonTable();
                         ?>
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
+            <!-- /.box-body -->
         </div>
-        <!-- /.box-body -->
+        <!-- /.box -->
     </div>
-    <!-- /.box -->
+    <!-- /.col -->
 </div>
 
 <!-- activate script -->
@@ -69,4 +70,3 @@ $(document).ready(function() {
     });
 });
 </script>
-
