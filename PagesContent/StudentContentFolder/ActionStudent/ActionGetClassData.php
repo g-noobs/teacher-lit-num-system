d4e105d53b87624a872e6edfbbb31904d7efb0c1
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-11-19 22:21:40 +0800

    asd

--- PagesContent/StudentContentFolder/ActionStudent/ActionGetClassData.php
+++ PagesContent/StudentContentFolder/ActionStudent/ActionGetClassData.php
@@ -8,16 +8,23 @@ $table = "view_teacher_class_info";
 
 
 $id = $_SESSION['id'];
-$sql = "SELECT class_id, class_name FROM $table WHERE class_status = 1 AND user_info_id = '$id';";
+$sql = "SELECT class_id, class_name, class_status FROM $table WHERE user_info_id = '$id';";
 
 $result = $conn->query($sql);
 
 if($result->num_rows > 0){
     while($row = $result->fetch_assoc()){
-        $response[] = array(
-            "id" => $row['class_id'],
-            "name" => $row['class_name']
-        );
+        if($row['class_status'] == 0){
+            $response[] = array(
+                "id" => $row['class_id'],
+                "name" => 'Class was archived'
+            );
+        }else{
+            $response[] = array(
+                "id" => $row['class_id'],
+                "name" => $row['class_name']
+            );
+        }
     }
     
 }
