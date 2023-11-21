025e8710fa8202f79a9aabd9860c231cf2bc51df
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-11-21 04:27:06 +0000

    ad

--- PagesContent/MainContentFolder/AllSciptMain/AssignDataScript.php
+++ PagesContent/MainContentFolder/AllSciptMain/AssignDataScript.php
@@ -5,3 +5,47 @@ $(function() {
 });
 </script>
 
+
+<script>
+$(function() {
+    $.ajax({
+        url: '../PagesContent/MainContentFolder/ActionMain/ActionPopulateAssignedClass.php',
+        type: 'GET',
+        dataType: 'json',
+        success: function(response) {
+            $.each(response, function(index, value) {
+                $('#assigned_class_list').append('<li>' + value +
+                    '</li>');
+            });
+
+        },
+        error: function(xhr, status, error) {
+            console.log(xhr);
+            $('#assigned_class_list').html(
+                '<li>Error loading content for assigned class</li>');
+        }
+    });
+});
+</script>
+
+<script>
+$(function() {
+    $.ajax({
+        url: '../PagesContent/MainContentFolder/ActionMain/ActionPopulateAssignedModule.php,
+        type: 'GET',
+        dataType: 'json',
+        success: function(response) {
+            $.each(response, function(index, value) {
+                $('#assigned_module_list').append('<li>' + value +
+                    '</li>');
+            });
+
+        },
+        error: function(xhr, status, error) {
+            console.log(xhr);
+            $('#assigned_class_list').html(
+                '<li>Error loading content for assigned class</li>');
+        }
+    });
+});
+</script>
