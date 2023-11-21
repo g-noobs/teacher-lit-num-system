befa302bc63217eee0106d3209281a31fd6abde9
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-11-21 03:00:02 +0000

    xc

--- CommonCode/header.php
+++ CommonCode/header.php
@@ -4,6 +4,8 @@ if ($_SESSION['teacher'] !== true || $_SESSION['admin'] !== false) {
     header('Location: ../../index.php');
     exit;
 }
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 ?>
 
 <header class="main-header">
