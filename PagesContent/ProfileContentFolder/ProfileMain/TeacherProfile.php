3a730a2538ea4dcb377a86d56a9bbe2c73a604e3
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-11-22 03:34:13 +0000

    as

--- PagesContent/ProfileContentFolder/ProfileMain/TeacherProfile.php
+++ PagesContent/ProfileContentFolder/ProfileMain/TeacherProfile.php
@@ -10,33 +10,33 @@
         <form action="post" id="teacher_profile">
             <div class="box-body" id="content_body">
                 <div class="form-group row">
-                    <div class="col-sm-5">
+                    <div class="col-xs-5">
                         <label for="teacher_id" col-form-label">Last Name</label>
                         <input type="text" class="form-control" id="last_name" placeholder="Last Name" required>
                     </div>
-                    <div class="col-sm-5">
+                    <div class="col-xs-5">
                         <label for="teacher_id" col-form-label">First Name</label>
                         <input type="text" class="form-control" id="first_name" placeholder="First Name" required>
                     </div>
-                    <div class="col-sm-2">
+                    <div class="col-xs-2">
                         <label for="teacher_id" col-form-label">M.I. (**optional)</label>
                         <input type="text" class="form-control" id="middle_initial" placeholder="Middle Initial"
                             maxlength="1" required>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <div class="col-sm-4">
+                    <div class="col-xs-4">
                         <label for="gender">Gender</label>
                         <select name="gemder" id="gender" class="form-control">
                             <option value="Male">Male</option>
                             <option value="Female">Female</option>
                         </select>
                     </div>
-                    <div class="col-sm-4">
+                    <div class="col-xs-4">
                         <label for="phone">Phone</label>
                         <input type="tel" name="phone" placeholder="Contact Number" class="form-control" required>
                     </div>
-                    <div class="col-sm-4">
+                    <div class="col-xs-4">
                         <label for="email">Email Address</label>
                         <input type="email" name="email" placeholder="Email Address" class="form-control" required>
                     </div>
@@ -45,38 +45,38 @@
                     <h4><strong>Address:</strong></h4>
                 </div>
                 <div class="form-group row">
-                    <div class="col-sm-6">
+                    <div class="col-xs-6">
                         <label for="street">Street</label>
                         <input type="text" name="street" placeholder="Street" class="form-control" required>
                     </div>
-                    <div class="col-sm-6">
+                    <div class="col-xs-6">
                         <label for="baranggay">Baranggay</label>
                         <input type="text" name="baranggay" placeholder="Baranggay" class="form-control" required>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <div class="col-sm-4">
+                    <div class="col-xs-4">
                         <label for="city_municipality">City /Municipal</label>
                         <input type="text" name="city_municipality" placeholder="City /Municipal" class="form-control"
                             required>
                     </div>
-                    <div class="col-sm-4">
+                    <div class="col-xs-4">
                         <label for="province">Province</label>
                         <input type="text" name="province" placeholder="Province" class="form-control" required>
                     </div>
-                    <div class="col-sm-4">
+                    <div class="col-xs-4">
                         <label for="zip_code">Zip Code</label>
                         <input type="number" name="zip_code" placeholder="Zip Code" class="form-control" required>
                     </div>
                 </div>
 
                 <div class="form-group row">
-                    <div class="col-sm-4">
+                    <div class="col-xs-4">
                         <label for="username:">Username: </label>
                         <input type="text" name="username" id="username" required="" class="form-control"
                             placeholder="Username" readonly="" disabled="">
                     </div>
-                    <div class="col-sm-4">
+                    <div class="col-xs-4">
                         <label for="password">Password</label>
                         <div class="input-group">
                             <input type="password" name="password" id="password" required="" class="form-control"
@@ -88,7 +88,7 @@
                             </span>
                         </div>
                     </div>
-                    <div class="col-sm-4">
+                    <div class="col-xs-4">
                         <label for="confirm_pass">Confirm Password</label>
                         <div class="input-group">
                             <input type="password" name="confirmPassword" id="confirmPassword" required=""
