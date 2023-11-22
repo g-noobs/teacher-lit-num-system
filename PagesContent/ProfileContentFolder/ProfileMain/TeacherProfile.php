df613abf0cb043502d8f88178ebe95762d3cadce
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-11-22 03:18:00 +0000

    asd

--- PagesContent/ProfileContentFolder/ProfileMain/TeacherProfile.php
+++ PagesContent/ProfileContentFolder/ProfileMain/TeacherProfile.php
@@ -21,9 +21,55 @@
                         </div>
                         <div class="col-sm-2">
                             <label for="teacher_id" col-form-label">M.I. (**optional)</label>
-                            <input type="text" class="form-control" id="middle_initial" placeholder="Last Name" required>
+                            <input type="text" class="form-control" id="middle_initial" placeholder="Last Name"
+                                required>
                         </div>
                     </div>
+                    <div class="form-group row">
+                        <div class="col-sm-4">
+                            <label for="gender">Gender</label>
+                            <select name="gemder" id="gender">
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                            </select>
+                        </div>
+                        <div class="col-sm-4">
+                            <label for="phone">Phone</label>
+                            <input type="tel" name="phone" placeholder="Contact Number" required>
+                        </div>
+                        <div class="col-sm-4">
+                            <label for="email">Email Address</label>
+                            <input type="email" name="email" placeholder="Email Address" required>
+                        </div>
+                    </div>
+                    <div class="container row">
+                        <h4><strong>Address:</strong></h4>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-6">
+                            <label for="street">Street</label>
+                            <input type="text" name="street" placeholder="Street" required>
+                        </div>
+                        <div class="col-sm-6">
+                            <label for="baranggay">Baranggay</label>
+                            <input type="text" name="baranggay" placeholder="Baranggay" required>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-4">
+                            <label for="city_municipality">City /Municipal</label>
+                            <input type="text" name="city_municipality" placeholder="City /Municipal" required>
+                        </div>
+                        <div class="col-sm-4">
+                            <label for="province">Province</label>
+                            <input type="text" name="province" placeholder="Province" required>
+                        </div>
+                        <div class="col-sm-4">
+                            <label for="zip_code">Zip Code</label>
+                            <input type="number" name="zip_code" placeholder="Zip Code" required>
+                        </div>
+                    </div>
+                    
                     <!-- <div class="col-md-6">
                         <div class="form-group">
                             <label for="teacher_last_name">Last Name</label>
