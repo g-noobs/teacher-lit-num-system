bb677fcea54e34df3a8bbf44442be530ff84bbca
g-noobs <102508567+g-noobs@users.noreply.github.com>
2023-11-22 03:27:20 +0000

    xcv

--- PagesContent/ProfileContentFolder/ProfileMain/TeacherProfile.php
+++ PagesContent/ProfileContentFolder/ProfileMain/TeacherProfile.php
@@ -8,97 +8,75 @@
                         class='glyphicon glyphicon-edit'></span></a></div>
         </div>
         <div class="box-body" id="content_body">
-            <div class="row">
-                <form action="post" id="teacher_profile">
-                    <div class="form-group row">
-                        <div class="col-sm-5">
-                            <label for="teacher_id" col-form-label">Last Name</label>
-                            <input type="text" class="form-control" id="last_name" placeholder="Last Name" required>
-                        </div>
-                        <div class="col-sm-5">
-                            <label for="teacher_id" col-form-label">First Name</label>
-                            <input type="text" class="form-control" id="first_name" placeholder="Last Name" required>
-                        </div>
-                        <div class="col-sm-2">
-                            <label for="teacher_id" col-form-label">M.I. (**optional)</label>
-                            <input type="text" class="form-control" id="middle_initial" placeholder="Last Name"
-                                required>
-                        </div>
+            <form action="post" id="teacher_profile">
+                <div class="form-group row">
+                    <div class="col-sm-5">
+                        <label for="teacher_id" col-form-label">Last Name</label>
+                        <input type="text" class="form-control" id="last_name" placeholder="Last Name" required>
                     </div>
-                    <div class="form-group row">
-                        <div class="col-sm-4">
-                            <label for="gender">Gender</label>
-                            <select name="gemder" id="gender" class="form-control" >
-                                <option value="Male">Male</option>
-                                <option value="Female">Female</option>
-                            </select>
-                        </div>
-                        <div class="col-sm-4">
-                            <label for="phone">Phone</label>
-                            <input type="tel" name="phone" placeholder="Contact Number" class="form-control" required>
-                        </div>
-                        <div class="col-sm-4">
-                            <label for="email">Email Address</label>
-                            <input type="email" name="email" placeholder="Email Address" class="form-control" required>
-                        </div>
+                    <div class="col-sm-5">
+                        <label for="teacher_id" col-form-label">First Name</label>
+                        <input type="text" class="form-control" id="first_name" placeholder="First Name" required>
                     </div>
-                    <div class="container row">
-                        <h4><strong>Address:</strong></h4>
+                    <div class="col-sm-2">
+                        <label for="teacher_id" col-form-label">M.I. (**optional)</label>
+                        <input type="text" class="form-control" id="middle_initial" placeholder="Middle Initial"
+                            maxlength="1" required>
                     </div>
-                    <div class="form-group row">
-                        <div class="col-sm-6">
-                            <label for="street">Street</label>
-                            <input type="text" name="street" placeholder="Street" class="form-control" required>
-                        </div>
-                        <div class="col-sm-6">
-                            <label for="baranggay">Baranggay</label>
-                            <input type="text" name="baranggay" placeholder="Baranggay" class="form-control" required>
-                        </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-sm-4">
+                        <label for="gender">Gender</label>
+                        <select name="gemder" id="gender" class="form-control">
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                        </select>
                     </div>
-                    <div class="form-group row">
-                        <div class="col-sm-4">
-                            <label for="city_municipality">City /Municipal</label>
-                            <input type="text" name="city_municipality" placeholder="City /Municipal" class="form-control" required>
-                        </div>
-                        <div class="col-sm-4">
-                            <label for="province">Province</label>
-                            <input type="text" name="province" placeholder="Province" class="form-control" required>
-                        </div>
-                        <div class="col-sm-4">
-                            <label for="zip_code">Zip Code</label>
-                            <input type="number" name="zip_code" placeholder="Zip Code" class="form-control" required>
-                        </div>
+                    <div class="col-sm-4">
+                        <label for="phone">Phone</label>
+                        <input type="tel" name="phone" placeholder="Contact Number" class="form-control" required>
                     </div>
+                    <div class="col-sm-4">
+                        <label for="email">Email Address</label>
+                        <input type="email" name="email" placeholder="Email Address" class="form-control" required>
+                    </div>
+                </div>
+                <div class="container row">
+                    <h4><strong>Address:</strong></h4>
+                </div>
+                <div class="form-group row">
+                    <div class="col-sm-6">
+                        <label for="street">Street</label>
+                        <input type="text" name="street" placeholder="Street" class="form-control" required>
+                    </div>
+                    <div class="col-sm-6">
+                        <label for="baranggay">Baranggay</label>
+                        <input type="text" name="baranggay" placeholder="Baranggay" class="form-control" required>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-sm-4">
+                        <label for="city_municipality">City /Municipal</label>
+                        <input type="text" name="city_municipality" placeholder="City /Municipal" class="form-control"
+                            required>
+                    </div>
+                    <div class="col-sm-4">
+                        <label for="province">Province</label>
+                        <input type="text" name="province" placeholder="Province" class="form-control" required>
+                    </div>
+                    <div class="col-sm-4">
+                        <label for="zip_code">Zip Code</label>
+                        <input type="number" name="zip_code" placeholder="Zip Code" class="form-control" required>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <button class="btn btn-primary" id="update-btn" type="submit">Update Profile</button>
+                </div>
 
-                    <!-- <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="teacher_last_name">Last Name</label>
-                            <input type="text" name="teacher_first_name" class="form-control" placeholder="Fist Name"
-                                required>
-                        </div>
-                        <div class="form-group">
-                            <label for="teacher_first_name">First Name</label>
-                            <input type="text" name="teacher_first_name" class="form-control" placeholder="Fist Name"
-                                required>
-                        </div>
-                        <div class="form-group">
-                            <label for="teacher_middle_initial">Middle Initial
-                                <span><small>*optional</small></span></label>
-                            <select name="teacher_middle_initial" id="teacher_middle_initial" class="form-control">
-                                <option value="">-</option>
-                            </select>
-                        </div>
-                        <div class="form-group">
+            </form>
 
-                        </div>
-                    </div> -->
 
-                    <!-- <div class="col-md-6"></div> -->
-                </form>
-            </div>
-            <div class="row">
-                <button class="btn btn-primary" id="update-btn" type="submit">Update Profile</button>
-            </div>
         </div>
     </div>
 
