4a2cef7705b7de010a6cc0c48f74b1cd6aacb011
Glen-Capstone <133980393+Glen-Capstone@users.noreply.github.com>
2023-11-24 03:49:01 +0000

    asd

--- PagesContent/ProfileContentFolder/ActionProfile/ActionPopulateData.php
+++ PagesContent/ProfileContentFolder/ActionProfile/ActionPopulateData.php
@@ -13,21 +13,23 @@ $sql = "SELECT * FROM $table WHERE user_info_id = '$id';";
 $result = $connection->getConnection()->query($sql);
 
 if($result->num_rows > 0){
-    $response = array(
-        'last_name' => $row['last_name'],
-        'first_name' => $row['first_name'],
-        'middle_initial' =>$row['middle_name'],
-        'gender' => $row['gender'],
-        'phone' => $row['contact_num'],
-        'email' => $row['email'],
-        'street' => $row['street'],
-        'baranggay' => $row['barangay'],
-        'city_municipality' => $row['municipal_city'],
-        'province' => $row['province'],
-        'zip_code' => $row['postalcode'],
-        'username' => $row['uname'],
-        'password' => $row['pass']
-    );
+    while($row = $result->fetch_assoc()) {
+        $response = array(
+            'last_name' => $row['last_name'],
+            'first_name' => $row['first_name'],
+            'middle_initial' =>$row['middle_name'],
+            'gender' => $row['gender'],
+            'phone' => $row['contact_num'],
+            'email' => $row['email'],
+            'street' => $row['street'],
+            'baranggay' => $row['barangay'],
+            'city_municipality' => $row['municipal_city'],
+            'province' => $row['province'],
+            'zip_code' => $row['postalcode'],
+            'username' => $row['uname'],
+            'password' => $row['pass']
+        );
+    }
 }else{
     $response = array('error' => 'No Data -- Error');
 }
