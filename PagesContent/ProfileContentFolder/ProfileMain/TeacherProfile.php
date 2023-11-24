2ae03e87d77ff5772f5c1a16661ec71f7fd5f934
Glen-Capstone <133980393+Glen-Capstone@users.noreply.github.com>
2023-11-24 06:56:22 +0000

    asd

--- PagesContent/ProfileContentFolder/ProfileMain/TeacherProfile.php
+++ PagesContent/ProfileContentFolder/ProfileMain/TeacherProfile.php
@@ -41,7 +41,8 @@
                     <div class="col-sm-4">
                         <label for="gender">Gender</label>
                         <select name="gender" id="gender" class="form-control">
-                            
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
                         </select>
                     </div>
                     <div class="col-sm-4">
