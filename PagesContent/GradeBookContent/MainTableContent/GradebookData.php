d1b6a5046b487c6503e9b3ce60c361299615783e
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-11-28 21:22:05 +0800

    asd

--- PagesContent/GradeBookContent/MainTableContent/GradebookData.php
+++ PagesContent/GradeBookContent/MainTableContent/GradebookData.php
@@ -57,7 +57,7 @@
 $(document).ready(function() {
     $("#export_btn").click(function() {
         let table = document.getElementsByTagName("table");
-        var fileName = $(this).data("name");
+        var fileName = $(this).data("gradebook");
         console.log(table);
         debugger;
         TableToExcel.convert(table[0], {
