4b5f0a5a4ad71fa301ff6f3fa56570e3177cc100
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-11-29 21:32:28 +0800

    asd

--- CommonCode/footer.php
+++ CommonCode/footer.php
@@ -3,7 +3,6 @@
         <div class="pull-right hidden-xs">
             <b>Version</b> 1
         </div>
-        <strong>2023 <a href="#" class="text-warning">Teacher Portal</a>.</strong> All rights
-        reserved.
+        
     </div>
 </footer>
