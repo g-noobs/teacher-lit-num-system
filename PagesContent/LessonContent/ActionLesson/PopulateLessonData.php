77134999144659dce86414b6aa361471a87c5524
Glen-Capstone <133980393+Glen-Capstone@users.noreply.github.com>
2023-12-02 06:30:32 +0000

    zxc

---
+++ PagesContent/LessonContent/ActionLesson/PopulateLessonData.php
@@ -0,0 +1,34 @@
+<?php 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+include_once "../../../Database/Connection.php";
+
+$connection = new Connection();
+$conn = $connection->getConnection();
+$table = "lesson_view";
+$lesson_id = $_POST['id'];
+
+if(!empty($lesson_id)){
+    $sql = "SELECT * FROM $table WHERE lesson_id = '$lesson_id';";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $response = array(
+                'lesson_name' => $row['lesson_name'],
+                'lesson_description' =>$row['lesson_description'],
+                // 'category_id' => $row['category_id'],
+                'module_id' => $row['module_id']
+            );
+        }
+    }else{
+        $response = array('error' => 'Table Empty');
+    }
+    //sent response to populate data for Edit
+    echo json_encode($response);
+    exit();
+}else{
+    $response = array('error' => 'No id received');
+    echo json_encode($response);
+    exit();
+}
+?>
--- PagesContent/LessonContent/LessonScript/EditLessonScript.php
+++ PagesContent/LessonContent/LessonScript/EditLessonScript.php
@@ -7,7 +7,38 @@ $('.edit').on('click', function(e) {
     var btn_id = $(this).data('id');
     $lesson_name = $('input[name="lesson_name"]');
     $lesson_description = $('textarea[name="lesson_description"]');
-    
+    $category_level = $('select[name="category_level"]');
+    //known as module
+    $subj_list = $('select[name="subj_list"]');
 
+    $.ajax({
+        type: "POST",
+        url: "../PagesContent/LessonContent/ActionLesson/PopulateLessonData.php",
+        data: {
+            id: btn_id
+        },
+        dataType: 'json',
+        success: function(response) {
+            $lesson_name.val(response.lesson_name);
+            $lesson_description.val(response.lesson_description);
+
+            // $category_level.find('option').each(function() {
+            //     if ($(this).val() === response.category_id) {
+            //         $(this).prop('selected', true);
+            //         return false;
+            //     }
+            // });
+            $subj_list.find('option').each(function() {
+                if ($(this).val() === response.module_id) {
+                    $(this).prop('selected', true);
+                    return false;
+                }
+            });
+            $modal.modal('show');
+        },
+        error: function() {
+            console.log('error');
+        }
+    });
 });
 </script>
