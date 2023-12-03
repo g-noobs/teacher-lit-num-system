7aed93a9c646afede9487ae8ee7c7ea4d95d0857
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-12-03 11:02:25 +0800

    asd

--- PagesContent/QuizFolder/QuizMainScript/EditQuizScript.php
+++ PagesContent/QuizFolder/QuizMainScript/EditQuizScript.php
@@ -1,6 +1,6 @@
 <script>
 $(function() {
-    $(".quiz_info_btn").on('click', function(e) {
+    $(".edit_quiz_btn").on('click', function(e) {
         e.preventDefault();
         $modal = $('#edit_quiz_modal');
         var btn_id = $(this).data('id');
