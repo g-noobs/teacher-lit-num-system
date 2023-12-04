ee969f005c9f85a9af503714d4621eb22a1b82b4
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-12-04 12:40:25 +0800

    asd

--- Database/DisplayStudentClass.php
+++ Database/DisplayStudentClass.php
@@ -58,7 +58,14 @@ class DisplayStudentClass extends Connection{
 
     }
     function assignClass($teacher_id){
-        $sql = "SELECT class_id, class_name FROM view_teacher_class_info WHERE class_status = 1 AND user_info_id = '$teacher_id';";
+        $table = "view_teacher_class_info";
+        // $sql = "SELECT class_id, class_name FROM view_teacher_class_info WHERE class_status = 1 AND user_info_id = '$teacher_id';";
+        $sql = "SELECT class_id, class_name FROM $table WHERE class_assign_status = 1 AND class_status = 1 AND user_info_id = '$teacher_id'
+        AND class_id IN (
+            SELECT class_id
+            FROM tbl_teacher_class_assignment
+            WHERE status = 1
+            );";
         $result = $this->getConnection()->query($sql);
         if($result->num_rows > 0){
             while($row = $result->fetch_assoc()){
