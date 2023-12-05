7a66248058e0b41e9cdb2fac6508373ed8bc9881
Glen-Capstone <133980393+Glen-Capstone@users.noreply.github.com>
2023-12-05 06:52:35 +0000

    as

--- CommonCode/header.php
+++ CommonCode/header.php
@@ -22,10 +22,11 @@ if ($_SESSION['teacher'] !== true || $_SESSION['admin'] !== false) {
                 </button>
             </div>
 
-            <div class="collapse navbar-collapse pull-left" id="navbar-collapse" aria-expanded="false" style="height: 0.8px;">
+            <div class="collapse navbar-collapse pull-left" id="navbar-collapse" aria-expanded="false"
+                style="height: 0.8px;">
                 <ul class="nav navbar-nav">
                     <li><a href="main.php">Dashboard <span class="sr-only">(current)</span></a></li>
-                    
+
                     <li><a href="class_list.php">Clas Management</a>
                         <!-- <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Class List <span
                                 class="caret"></span></a>
@@ -42,10 +43,10 @@ if ($_SESSION['teacher'] !== true || $_SESSION['admin'] !== false) {
                             <li class="divider"></li>
                             <li><a href="quiz.php">Quiz</a></li>
                         </ul>
-                        <li><a href="gradebook.php">Gradebook</a></li>
+                    <li><a href="gradebook.php">Gradebook</a></li>
                     </li>
                 </ul>
-                
+
                 <!-- Search Bar
                 <form class="navbar-form navbar-left" role="search">
                     <div class="form-group">
@@ -58,7 +59,48 @@ if ($_SESSION['teacher'] !== true || $_SESSION['admin'] !== false) {
 
             <div class="navbar-custom-menu">
                 <ul class="nav navbar-nav">
-                
+                    <li class="dropdown notifications-menu">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <i class="fa fa-bell-o"></i>
+                            <span class="label label-warning">10</span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li class="header">You have 10 notifications</li>
+                            <li>
+                                <ul class="menu">
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa fa-users text-aqua"></i> 5 new members joined today
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa fa-warning text-yellow"></i> Very long description here that
+                                            may not fit into the
+                                            page and may cause design problems
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa fa-users text-red"></i> 5 new members joined
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa fa-shopping-cart text-green"></i> 25 sales made
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa fa-user text-red"></i> You changed your username
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="footer"><a href="#">View all</a></li>
+                        </ul>
+                    </li>
+                    <!-- end of notification  -->
                     <li class="dropdown user user-menu">
 
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
@@ -74,7 +116,8 @@ if ($_SESSION['teacher'] !== true || $_SESSION['admin'] !== false) {
                         </a>
                         <ul class="dropdown-menu">
                             <li class="user-header">
-                                <img src="../Media/Images/UserAvatar/temp_profpic.png" class="img-circle" alt="User Image">
+                                <img src="../Media/Images/UserAvatar/temp_profpic.png" class="img-circle"
+                                    alt="User Image">
                                 <p id=teacher_name>
                                     <?php $displayUserInfo->displayTeacherName();?>
                                 </p>
