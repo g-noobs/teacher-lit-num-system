9b66820e976f23e996a2a13a98c6fb6040738bb5
Glen-Capstone <133980393+Glen-Capstone@users.noreply.github.com>
2023-12-05 06:46:32 +0000

    xx

--- pages/gradebook.php
+++ pages/gradebook.php
@@ -63,7 +63,7 @@
                         </div>
                     </div>
                 </section>
-
+                <br>
                 <!-- Main Content-->
                 <section class="content" id="gradebook_content">
                     <?php include_once "../PagesContent/GradeBookContent/MainTableContent/GradebookData.php"?>
