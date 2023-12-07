998362bebd54c2e2e713bf72f8d4c9c5cb9018a2
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-12-07 18:01:26 +0800

    zx

--- PagesContent/AssignmentFolder/ScriptAssignment/AssignedTableScript.php
+++ PagesContent/AssignmentFolder/ScriptAssignment/AssignedTableScript.php
@@ -5,7 +5,7 @@ $(function() {
         //empty and id
         $('#assign_content').empty();
         //add new content that on php file
-        $('#assign_content').load(<?php include_once "../PagesContent/AssignmentFolder/MainAssignment/AssignedPanel.php"?>);
+        $('#assign_content').load("../PagesContent/AssignmentFolder/MainAssignment/AssignedPanel.php");
     });
 });
 </script>
