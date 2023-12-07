e789829b880b533610d334555b0b7e01ecda71bb
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-12-07 18:13:25 +0800

    sd

--- PagesContent/AssignmentFolder/ScriptAssignment/AssignedTableScript.php
+++ PagesContent/AssignmentFolder/ScriptAssignment/AssignedTableScript.php
@@ -3,11 +3,11 @@ $(function() {
     $(document).on('click', '#assigned_assgn_btn', function(e) {
         e.preventDefault();
         //empty and id
-        $('#assign_content').fadeOut(400), function() { 
+        $('#assign_content').empty().fadeOut(400, function() { 
             $(this).load("../PagesContent/AssignmentFolder/MainAssignment/AssignedPanel.php", function() {
                 $(this).fadeIn(400);
             });
-        }
+        });
     });
 });
 </script>
