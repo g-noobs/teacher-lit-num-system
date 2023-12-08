838ba8d5f481f5f20472fe719cb1057ff627078f
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-12-08 21:23:38 +0800

    asd

---
+++ PagesContent/GradeBookContent/MainTableContent/LessonProgress.php
@@ -0,0 +1,67 @@
+<div class="row">
+    <div class="col-xs-12">
+        <div class="box box-default container">
+            <br>
+            <div class="box-header with-">
+
+                <h3 class="box-title"> Learner Story Progress for <span id="user_name"></span></h3>
+                <h4 id="personal_id"></h4>
+                <select id='filterSelect' onchange='applyFilter()'>
+                    <option value='all'>All</option>
+                    <option value='completed'>Completed</option>
+                    <option value='not_completed'>Not Completed</option>
+                </select>
+                <div class="box-tools pull-right">
+                    <div class="search-box" style="margin-right: 35px;">
+                        <i class="fa fa-search"></i>
+                        <input type="text" id="userInput" class="form-control" placeholder="Search..">
+                    </div>
+                </div>
+                <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                    </button>
+                </div>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body" style="overflow-y: scroll; max-height: 400px;">
+                <!-- export button -->
+                <!-- Table Data -->
+                <div class="table-responsive">
+                    <table id="progressTable" class="table table-bordered table-hover text-center">
+                        <thead>
+
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+    </div>
+    <!-- /.col -->
+</div>
+
+<script>
+function applyFilter() {
+    var filter = document.getElementById('filterSelect').value;
+    var rows = document.getElementsByClassName('progressRow');
+
+    for (var i = 0; i < rows.length; i++) {
+        var status = rows[i].getAttribute('data-status');
+
+        if (filter === 'all' || (filter === 'completed' && status.includes('Completed')) || (filter ===
+                'not_completed' && status.includes('Not Yet Taken'))) {
+            rows[i].style.display = 'table-row';
+        } else {
+            rows[i].style.display = 'none';
+        }
+    }
+}
+
+function goBack() {
+    window.history.back();
+}
+</script>
---
+++ PagesContent/GradeBookContent/MainTableContent/QuizProgress.php
@@ -0,0 +1,39 @@
+<div class="row">
+    <div class="col-xs-12">
+        <div class="box box-default container">
+            <br>
+            <div class="box-header with-">
+
+                <h3 class="box-title"></h3>
+                <div class="box-tools pull-right">
+                    <div class="search-box" style="margin-right: 35px;">
+                        <i class="fa fa-search"></i>
+                        <input type="text" id="userInput" class="form-control" placeholder="Search..">
+                    </div>
+                </div>
+                <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                    </button>
+                </div>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body" style="overflow-y: scroll; max-height: 400px;">
+                <!-- export button -->
+                <!-- Table Data -->
+                <div class="table-responsive">
+                    <table id="lesson_progress.php" class="table table-bordered table-hover text-center">
+                        <thead>
+
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+    </div>
+    <!-- /.col -->
+</div>
