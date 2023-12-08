7e81af59819df0d03b0877ef874c74d4a2c8c27a
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-12-08 20:45:20 +0800

    asd

--- PagesContent/GradeBookContent/ModalGradeBook/FilterModal.php
+++ PagesContent/GradeBookContent/ModalGradeBook/FilterModal.php
@@ -8,19 +8,21 @@
                 <h4 class="modal-title" id="modal-title">Filter Table View</h4>
             </div>
             <div class="modal-body">
-                <label><input type="checkbox" class="chkUserInfoID"> User Info ID</label>
-                <label><input type="checkbox" class="chkPersonalID"> Personal ID</label>
-                <label><input type="checkbox" class="chkFirstName"> First Name</label>
-                <label><input type="checkbox" class="chkLastName"> Last Name</label>
-                <label><input type="checkbox" class="chkGender"> Gender</label>
-                <label><input type="checkbox" class="chkClassSection"> Class Section</label>
-                <label><input type="checkbox" class="chkTopicsTaken"> Topics Taken</label>
-                <label><input type="checkbox" class="chkQuizTaken"> Quiz Taken</label>
-                <label><input type="checkbox" class="chkLearnerProgress"> Learner Story Progress</label>
-                <label><input type="checkbox" class="chkQuizProgress"> Quiz Progress</label>
-                <button onclick="applyFilter()">Apply</button>
-                <button onclick="resetTable()">Reset</button>
-                <button onclick="closeFilterModal()">Close</button>
+                <div class="box-body box-warning">
+                    <label><input type="checkbox" class="chkUserInfoID"> User Info ID</label>
+                    <label><input type="checkbox" class="chkPersonalID"> Personal ID</label>
+                    <label><input type="checkbox" class="chkFirstName"> First Name</label>
+                    <label><input type="checkbox" class="chkLastName"> Last Name</label>
+                    <label><input type="checkbox" class="chkGender"> Gender</label>
+                    <label><input type="checkbox" class="chkClassSection"> Class Section</label>
+                    <label><input type="checkbox" class="chkTopicsTaken"> Topics Taken</label>
+                    <label><input type="checkbox" class="chkQuizTaken"> Quiz Taken</label>
+                    <label><input type="checkbox" class="chkLearnerProgress"> Learner Story Progress</label>
+                    <label><input type="checkbox" class="chkQuizProgress"> Quiz Progress</label>
+                    <button onclick="applyFilter()">Apply</button>
+                    <button onclick="resetTable()">Reset</button>
+                    <button onclick="closeFilterModal()">Close</button>
+                </div>
             </div>
         </div>
         <!-- /.modal-content -->
