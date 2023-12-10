70048883e96ebbceb2270b1460c80cf1d45b23dc
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-12-10 22:38:07 +0800

    zxc

--- PagesContent/GradeBookContent/GradebookScripts/AddToInterventionScript.php
+++ PagesContent/GradeBookContent/GradebookScripts/AddToInterventionScript.php
@@ -10,49 +10,56 @@ $(function() {
         $('#studername_intervention').empty();
         $('#studername_intervention').text($student_name);
         var $modal = $('#intervention_modal');
+        $modal.modal('show');
 
-        $.ajax({
-            type: 'POST',
-            url: '../PagesContent/GradeBookContent/ActionGradebook/ActionAddIntervention.php',
-            data: {
-                id: $id
-            },
-            success: function(response) {
-                var responseData = JSON.parse(response);
-                // Check if the form submission was successful
-                if (responseData.hasOwnProperty('success')) {
-                    $modal.modal('hide');
-                    $('#successAlert').text(responseData.success);
-                    $('#successBanner').show();
-                    setTimeout(function() {
-                        $("#successBanner").fadeOut("slow");
-                        location.reload();
-                    }, 5500);
+        $(document).on('click', '#confirm_intervention', function() {
+            //Ajax code
+            // var action_url =
+            //     "../PagesContent/GradeBookContent/ActionGradebook/ActionAddIntervention.php";
+
+            
+        // $.ajax({
+        //     type: 'POST',
+        //     url: '../PagesContent/GradeBookContent/ActionGradebook/ActionAddIntervention.php',
+        //     data: {
+        //         id: $id
+        //     },
+        //     success: function(response) {
+        //         var responseData = JSON.parse(response);
+        //         // Check if the form submission was successful
+        //         if (responseData.hasOwnProperty('success')) {
+        //             $modal.modal('hide');
+        //             $('#successAlert').text(responseData.success);
+        //             $('#successBanner').show();
+        //             setTimeout(function() {
+        //                 $("#successBanner").fadeOut("slow");
+        //                 location.reload();
+        //             }, 5500);
 
 
-                    // You can redirect to a different page or perform other actions here
-                } else if (responseData.hasOwnProperty('error')) {
-                    $modal.modal('hide');
-                    $('#errorAlert').text(responseData.error);
-                    $('#errorBanner').show();
-                    setTimeout(function() {
-                        $("#errorBanner").fadeOut("slow");
-                        location.reload();
-                    }, 5500);
-                }
-            },
-            error: function() {
-                $modal.modal('hide');
-                //show alert banner id = errorBanner
-                $('#errorAlert').text(
-                    'An error occurred during the AJAX request.');
-                $('#errorBanner').show();
-                setTimeout(function() {
-                    $("#errorBanner").fadeOut("slow");
-                    location.reload();
-                }, 5500);
-            }
-        });
+        //             // You can redirect to a different page or perform other actions here
+        //         } else if (responseData.hasOwnProperty('error')) {
+        //             $modal.modal('hide');
+        //             $('#errorAlert').text(responseData.error);
+        //             $('#errorBanner').show();
+        //             setTimeout(function() {
+        //                 $("#errorBanner").fadeOut("slow");
+        //                 location.reload();
+        //             }, 5500);
+        //         }
+        //     },
+        //     error: function() {
+        //         $modal.modal('hide');
+        //         //show alert banner id = errorBanner
+        //         $('#errorAlert').text(
+        //             'An error occurred during the AJAX request.');
+        //         $('#errorBanner').show();
+        //         setTimeout(function() {
+        //             $("#errorBanner").fadeOut("slow");
+        //             location.reload();
+        //         }, 5500);
+        //     }
+        // });
 
 
     });
