83944740842002e3f4a2fbcc54ad1938a3894d82
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-12-10 22:14:27 +0800

    zxc

--- PagesContent/GradeBookContent/AllModal/ModalIntervention.php
+++ PagesContent/GradeBookContent/AllModal/ModalIntervention.php
@@ -1,11 +1,11 @@
 <!-- Intervention Modal -->
-<div class="modal fade" id="archive_modal" role="dialog">
+<div class="modal fade" id="intervention_modal" role="dialog">
     <div class="modal-dialog modal-sm">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Are you sure to send thsi student for Intervention?</h4>
-                <h5>Studeent Name: <span id="studername_intervention"></span></h5>
+                <h4 class="modal-title">Are you sure to send this student for Intervention?</h4>
+                <h5>Student Name: <span id="studername_intervention"></span></h5>
 
             </div>
             <div class="modal-body">
