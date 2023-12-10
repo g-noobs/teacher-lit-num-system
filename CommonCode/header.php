4f02ec63dbc88b5fd6645e3b42c21c42aadaafdd
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-12-10 21:57:21 +0800

    asd

--- CommonCode/header.php
+++ CommonCode/header.php
@@ -48,6 +48,7 @@ if ($_SESSION['teacher'] !== true || $_SESSION['admin'] !== false) {
 
                         </ul>
                     <li><a href="gradebook.php">Gradebook</a></li>
+                    <li><a href="intervention_tickets.php">Intervention</a></li>
                     </li>
                 </ul>
 
