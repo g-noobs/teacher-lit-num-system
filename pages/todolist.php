cba0720c2e00369fcb5405e8a413e3134405000f
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-12-11 10:40:35 +0800

    asd

--- pages/todolist.php
+++ pages/todolist.php
@@ -56,6 +56,8 @@ $result = mysqli_query($connection, $query);
     style="height: auto; min-height: 100%;">
 
     <div class="wrapper" style="height: auto; min-height: 100%;">
+    <?php include_once("../CommonCode/header.php");?>
+
         <div class="content-wrapper" style="min-height: 606.2px;">
             <?php include_once "../CommonCode/ModifiedAlert.php";?>
             <div class="container">
