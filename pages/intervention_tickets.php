256260e12d4bf88209a4024d215d4a62f5c99520
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-12-11 23:10:37 +0800

    ads

--- pages/intervention_tickets.php
+++ pages/intervention_tickets.php
@@ -186,22 +186,6 @@ $filterClass = isset($_GET['status']) ? $_GET['status'] : 'all';
         }
     }
     </script>
-    <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $currentDate = date('Y-m-d');
-        $status = mysqli_real_escape_string($connection, $_POST['status']);
-
-        $updateQuery = "UPDATE tbl_intervention SET status = '1', end_date = '$currentDate' WHERE status = '0'";
-
-        if (mysqli_query($connection, $updateQuery)) {
-            echo "Data updated successfully!";
-        } else {
-            echo "Error: " . $updateQuery . "<br>" . mysqli_error($connection);
-        }
-    }
-
-    mysqli_close($connection);
-    ?>
 
     <?php include_once("../bootstrap/jquery.php");?>
     <!-- This Script Contain Common Script used on other pages  -->
