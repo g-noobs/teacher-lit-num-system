1eb3de52585a93cb118714e0089d9053cabda78f
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-12-11 13:33:36 +0800

    zxc

--- PagesContent/InterventionContents/ActionIntervention.php
+++ PagesContent/InterventionContents/ActionIntervention.php
@@ -2,6 +2,21 @@
 session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+include_once "../../../Database/SanitizeCrudClass.php";
 
+$table = "tbl_intervention";
+
+$id = $_GET['id'];
+$sql = "UPDATE $table SET status = 1 WHERE intervention_id = ?;";
+$params = array($id);
+$removeIntervention = new SanitizeCrudClass();
+$removeIntervention->executePreState($sql, $params);
+if($removeIntervention->getLastError() == null){
+    echo json_encode(array('success' => 'Intervention Removed Successfully'));
+    exit();
+}else{
+    echo json_encode(array('error' => 'Intervention Failed to Remove'));
+    exit();
+}
 
 ?>
