35107d79d6ebfaa7e51368b1c10165637561a518
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-12-11 10:39:35 +0800

    xcv

--- pages/todolist.php
+++ pages/todolist.php
@@ -45,34 +45,70 @@ $result = mysqli_query($connection, $query);
         text-align: left;
     }
     </style>
+    <?php include_once("../bootstrap/style.php");?>
+    <script src="../design/bower_components/jquery/dist/jquery.min.js"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="../design/bower_components/jquery-ui/jquery-ui.min.js"></script>
+    <?php include_once "../CommonCode/ModifiedSearchStyle.php";?>
 </head>
 
-<body>
+<body class="skin-blue layout-top-nav fixed" data-new-gr-c-s-check-loaded="14.1131.0" data-gr-ext-installed
+    style="height: auto; min-height: 100%;">
 
-    <h2>To do list</h2>
+    <div class="wrapper" style="height: auto; min-height: 100%;">
+        <div class="content-wrapper" style="min-height: 606.2px;">
+            <?php include_once "../CommonCode/ModifiedAlert.php";?>
+            <div class="container">
+                <section class="content-header">
+                    <h1>
+                        Gradebook
+                        <small>Teacher Portal</small>
+                    </h1>
+                </section>
+                <section class="content" id="main_content">
 
-    <table class="to-do-list-board-container">
-        <tr>
-            <th>Student Name</th>
-            <th>Class Section</th>
-            <th>Assignment Name</th>
-            <th class="assignmentProgress">Action</th>
-        </tr>
+                    <div class="row" id="gradebook_content">
+                        <div class="col-xs-12">
+                            <div class="box box-default container">
+                                <br>
+                                <div class="box-header with-">
+                                    <h3 class="box-title">To do list</h3>
+                                </div>
+                                <div class="box-body" style="overflow-y: scroll; max-height: 400px;">
+                                    <div class="table-responsive">
+                                        <table id="userTable"
+                                            class="table table-bordered table-hover text-center to-do-list-board-container">
+                                            <tr>
+                                                <th>Student Name</th>
+                                                <th>Class Section</th>
+                                                <th>Assignment Name</th>
+                                                <th class="assignmentProgress">Action</th>
+                                            </tr>
 
-        <?php
-    // table result para sa name class name ug assignment name
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "<tr>";
-        echo "<td>" . $row['fullname'] . "</td>";
-        echo "<td>" . $row['class_name'] . "</td>";
-        echo "<td>" . $row['assignment_name'] . "</td>"; 
-        echo "<td class='assignmentProgress'><button onclick=\"showAssignmentProgress('{$row['user_info_id']}')\">show assignment</button></td>";
-        echo "</tr>";
-    }
-    ?>
+                                            <?php
+                                                // table result para sa name class name ug assignment name
+                                                while ($row = mysqli_fetch_assoc($result)) {
+                                                    echo "<tr>";
+                                                    echo "<td>" . $row['fullname'] . "</td>";
+                                                    echo "<td>" . $row['class_name'] . "</td>";
+                                                    echo "<td>" . $row['assignment_name'] . "</td>"; 
+                                                    echo "<td class='assignmentProgress'><button onclick=\"showAssignmentProgress('{$row['user_info_id']}')\">show assignment</button></td>";
+                                                    echo "</tr>";
+                                                }
+                                            ?>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
 
-    </table>
-
+        </div>
+        <?php include_once("../CommonCode/footer.php");?>
+    </div>
+    <?php include_once("../bootstrap/jquery.php");?>
     <script>
     function showAssignmentProgress(userId) {
         window.location.href = "get_assignment_progress.php?userId=" + userId;
