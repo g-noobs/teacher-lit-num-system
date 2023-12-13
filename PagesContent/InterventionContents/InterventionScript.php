2d342e81edce31afa1a6fe3be292acded5057fe8
Glen-Capstone <133980393+Glen-Capstone@users.noreply.github.com>
2023-12-13 04:17:43 +0000

    sd

--- PagesContent/InterventionContents/InterventionScript.php
+++ PagesContent/InterventionContents/InterventionScript.php
@@ -1,29 +1,53 @@
 <script>
 $(function() {
-    $(document).on('change', '.status_update', function(e){
+    $(document).on('change', '.status_update', function(e) {
         e.preventDefault();
-         // Get the selected value
+        // Get the selected value
         var selectedValue = $(this).val();
 
         // Get the intervention_id from the data-id attribute
         var interventionId = $(this).data('id');
         var actionUrl = '';
 
-        console.log("Received value: "+ selectedValue +" Received intervention ID: "+ interventionId);
-        // $.ajax({
-        //     type: 'POST',
-        //     url: actionUrl,
-        //     data:{
-
-        //     },
-        //     dataType: 'json',
-        //     success:function(response){
-
-        //     },
-        //     error: function(repsonse){
-
-        //     }
-        // });
+        console.log("Received value: " + selectedValue + " Received intervention ID: " +
+            interventionId);
+        $.ajax({
+            type: 'POST',
+            url: actionUrl,
+            data: {
+                id: interventionId,
+                status: selectedValue
+            },
+            dataType: 'json',
+            success: function(response) {
+                // Check if the response contains an array of errors
+                if (response.hasOwnProperty('success')) {
+                    $('#successAlert').text(response.success);
+                    $('#successBanner').show();
+                    setTimeout(function() {
+                        $("#successBanner").fadeOut("slow");
+                        location.reload();
+                    }, 1500);
+                } else if (response.hasOwnProperty('error')) {
+                    $('#errorAlert').text(response.error);
+                    $('#errorBanner').show();
+                    setTimeout(function() {
+                        $("#errorBanner").fadeOut("slow");
+                        location.reload();
+                    }, 5500);
+                }
+            },
+            error: function(xhr, status, error) {
+                console.error(xhr.responseText);
+                //show alert banner id = errorBanner
+                $('#errorAlert').text('An error occurred during the AJAX request.');
+                $('#errorBanner').show();
+                setTimeout(function() {
+                    $("#errorBanner").fadeOut("slow");
+                    location.reload();
+                }, 1500);
+            }
+        });
     });
 });
 </script>
@@ -36,7 +60,7 @@ $(function() {
         $modal = $('#add_intervention_modal').show();
         $modal.modal('show');
 
-        $(document).on('submit', '#intervention_form', function(e){
+        $(document).on('submit', '#intervention_form', function(e) {
             e.preventDefault();
             var formData = new FormData(this);
             var actionUrl = '../PagesContent/InterventionContents/ActionAddIntervention.php';
