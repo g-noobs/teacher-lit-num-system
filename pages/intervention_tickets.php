3d52f7344e9b7481b18001c77ac64b1c2cd6ba2e
g-noobs <paragsa.223017@gensan.sti.edu.ph>
2023-12-14 08:03:00 +0800

    xcv

--- pages/intervention_tickets.php
+++ pages/intervention_tickets.php
@@ -89,7 +89,6 @@ $filterClass = isset($_GET['status']) ? $_GET['status'] : 'all';
                                             if ($filterClass !== '') {
                                                 $userInfoQuery .= " WHERE status = '$filterClass'";
                                             }
-                                            session_start();
                                             $teacher_id = $_SESSION['id'];
                                             $userInfoQuery .= " AND view_intervention.added_byID = '$teacher_id'";
 
